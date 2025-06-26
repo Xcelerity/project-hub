0d72d2d4ab481ef0522cd8734e32df96b31693f3
Xcelerity <kchiruhaas@gmail.com>
2025-06-26 13:30:01 +0530

    New Commit

--- portfolio/main.py
+++ portfolio/main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
@@ -15,3 +17,21 @@ with col2:
 
 p2 = """Here, you’ll find some of the the Python apps and projects I’ve built!"""
 st.write(p2)
+
+df = pd.read_csv("info.csv", sep = ";")
+
+col3,space,col4 = st.columns([1.5,0.5,1.5])
+
+with col3:
+    for index, row in df.iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(row["image"], width=200)
+        st.write(f"[Source Code]{row['url']}")
+
+with col4:
+    for index, row in df.iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(row["image"], width=200)
+        st.write(f"[Source Code]{row['url']}")
